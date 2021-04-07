4294ff5368766db88fe3efbf0e6f11c3a4d58c79
dell <hishamrana00@gmail.com>
2021-04-07 10:29:15 +0200

    changes are done

---
+++ app/src/main/java/com/rana/tiptime/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.rana.tiptime
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rana.tiptime.R.string
+import com.rana.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // binding.myButton.text = "Calculate Tip"
+
+        binding.myButton.setOnClickListener { calculateTip() }
+
+    }
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    fun calculateTip() {
+
+   // var cost = ٍStringInTextField.toDoubleOrNull()
+        val stringInTextField = binding.costOfService.text.toString()
+         val cost = stringInTextField.toDoubleOrNull()
+
+        if (cost == null){
+            binding.tipResult.text= " "
+            return
+        }
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage*cost
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+       binding.tipResult.text =tip.toString()
+//
+    }
+  //======================================
+
+
+
+
+
+
+
+
+
+
+}
+    /////////////////////////////////////////////////////////////////////////////////////
+
+
+
